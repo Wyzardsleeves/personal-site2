2ae96633f24cdb302fda9447f525b7f47e41709e
JayLatt <jlattimor3@gmail.com>
2017-07-23 19:18:13 -0400

    changed names of portfolio apps

--- scripts/project_fixtures.js
+++ scripts/project_fixtures.js
@@ -1,29 +1,29 @@
-var blocPong = {
-  title: 'Bloc Pong',
+var mockPong = {
+  title: 'Mock Pong',
   description: 'Pong game made with html5 canvas.',
-  projectImageUrl: 'assets/blocPong.png',
+  projectImageUrl: 'assets/mockPong.png',
   projectUrl: 'https://wyzardsleeves.github.io/bloc-pong/'
 };
 
-var blocItOff = {
-  title: 'Bloc It Off',
+var mockItOff = {
+  title: 'Mock It Off',
   description: 'An Angularfire powered checklist.',
-  projectImageUrl: 'assets/blocItOff.png',
+  projectImageUrl: 'assets/mockItOff.png',
   projectUrl: 'https://lit-everglades-16755.herokuapp.com/'
 };
 
-var blocChat = {
-   title: 'Bloc Chat',
+var mockChat = {
+   title: 'Mock Chat',
    description: 'An Angularfire powered chatroom.',
-   projectImageUrl: 'assets/blocChat.png',
+   projectImageUrl: 'assets/mockChat.png',
    projectUrl: 'https://fathomless-sands-94333.herokuapp.com/'
 };
 
-var blocipedia = {
-  title: 'Blocipedia (under contruction)',
+var mockipedia = {
+  title: 'Mockipedia (under contruction)',
   description: 'Ruby on Rails Wikipedia-ish app.',
-  projectImageUrl: 'assets/blocipedia.png',
+  projectImageUrl: 'assets/mockipedia.png',
   projectUrl: 'https://blocipedia-wyzard.herokuapp.com/'
 };
 
-var projectEntries = [blocPong, blocItOff, blocChat, blocipedia];
+var projectEntries = [mockPong, mockItOff, mockChat, mockipedia];
